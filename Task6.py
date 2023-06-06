53ccc416c648a32ab412c272157ab409c1f9c432
VladyslavSokolov <131270644+VladyslavSokolov@users.noreply.github.com>
2023-06-06 18:35:24 +0200

    Add files via upload

---
+++ Task6.py
@@ -0,0 +1,8 @@
+def load_xml(file_path):
+    try:
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        return root
+    except ET.ParseError as e:
+        print(f"Błąd w pliku XML: {e}")
+        return None
