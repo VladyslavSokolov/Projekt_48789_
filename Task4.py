ad349a3a566ae3a74fb28048309f83a495683b31
VladyslavSokolov <131270644+VladyslavSokolov@users.noreply.github.com>
2023-06-06 18:34:46 +0200

    Add files via upload

---
+++ Task4.py
@@ -0,0 +1,10 @@
+import yaml
+
+def load_yaml(file_path):
+    with open(file_path, 'r') as file:
+        try:
+            data = yaml.safe_load(file)
+            return data
+        except yaml.YAMLError as e:
+            print(f"Błąd w pliku YAML: {e}")
+            return None
