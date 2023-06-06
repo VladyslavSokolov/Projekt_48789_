302600a02ab6540e17831a27ee5dc40a89dcae04
VladyslavSokolov <131270644+VladyslavSokolov@users.noreply.github.com>
2023-06-06 18:34:02 +0200

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,10 @@
+import json
+
+def load_json(file_path):
+    with open(file_path, 'r') as file:
+        try:
+            data = json.load(file)
+            return data
+        except json.JSONDecodeError as e:
+            print(f"Błąd w pliku JSON: {e}")
+            return None
